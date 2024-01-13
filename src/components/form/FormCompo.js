14c166590ea19b9125876f8953395008b1b70c54
alshohid <alshohid1997@gmail.com>
2024-01-13 19:42:46 +0600

    added

--- src/components/form/FormCompo.js
+++ src/components/form/FormCompo.js
@@ -1,9 +1,69 @@
 'use client'
+
+import Image from "next/image";
+import Styles from './formCompo.module.css'
  
   
   const FormCompo = () => {
     return (
-      <div>
+      <div className="bg-[#FFFFFF]">
+        <div className="flex justify-center align-middle">  <Image
+          src={`https://cdn.ostad.app/public/upload/2023-08-31T05-57-04.987Z-image49.svg`}
+           width={80}
+           height={70}
+         /></div>
+        
+         <div className="text-center">
+            <h3 className="mb-0-5 md:text-[18px] sm:text-[16px] lg:text-[26px]"> প্রোডাক্ট  সম্পর্কে আপনার মতামত দিন</h3>
+            <p style={{fontWeight: '400',fontSize: '16px',lineHeight: '150%' , letterSpacing:  '.02em'}} >নিচের স্টারগুলো থেকে  প্রোডাক্ট সম্পর্কে আমাদের সার্ভিসকে রেটিং দিন</p>
+         </div>
+
+       <div className={Styles.ratingContainer} >
+        <div className="cursor-pointer pr-0-5">
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+        <path d="M24 7.23607L27.5392 18.1287L27.7637 18.8197H28.4903H39.9434L30.6776 25.5517L30.0899 25.9787L30.3144 26.6697L33.8536 37.5623L24.5878 30.8303L24 30.4033L23.4122 30.8303L14.1464 37.5623L17.6856 26.6697L17.9101 25.9787L17.3224 25.5517L8.05655 18.8197H19.5097H20.2363L20.4608 18.1287L24 7.23607Z" stroke="#FFAB00" stroke-width="2" fill="">
+        </path>
+        </svg>
+        </div>
+
+        <div className="cursor-pointer pr-0-5">
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+        <path d="M24 7.23607L27.5392 18.1287L27.7637 18.8197H28.4903H39.9434L30.6776 25.5517L30.0899 25.9787L30.3144 26.6697L33.8536 37.5623L24.5878 30.8303L24 30.4033L23.4122 30.8303L14.1464 37.5623L17.6856 26.6697L17.9101 25.9787L17.3224 25.5517L8.05655 18.8197H19.5097H20.2363L20.4608 18.1287L24 7.23607Z" stroke="#FFAB00" stroke-width="2" fill="">
+        </path>
+        </svg>
+        </div>
+
+
+        <div className="cursor-pointer pr-0-5">
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+        <path d="M24 7.23607L27.5392 18.1287L27.7637 18.8197H28.4903H39.9434L30.6776 25.5517L30.0899 25.9787L30.3144 26.6697L33.8536 37.5623L24.5878 30.8303L24 30.4033L23.4122 30.8303L14.1464 37.5623L17.6856 26.6697L17.9101 25.9787L17.3224 25.5517L8.05655 18.8197H19.5097H20.2363L20.4608 18.1287L24 7.23607Z" stroke="#FFAB00" stroke-width="2" fill="">
+        </path>
+        </svg>
+        </div>
+
+        <div className="cursor-pointer pr-0-5">
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+        <path d="M24 7.23607L27.5392 18.1287L27.7637 18.8197H28.4903H39.9434L30.6776 25.5517L30.0899 25.9787L30.3144 26.6697L33.8536 37.5623L24.5878 30.8303L24 30.4033L23.4122 30.8303L14.1464 37.5623L17.6856 26.6697L17.9101 25.9787L17.3224 25.5517L8.05655 18.8197H19.5097H20.2363L20.4608 18.1287L24 7.23607Z" stroke="#FFAB00" stroke-width="2" fill="">
+        </path>
+        </svg>
+        </div>
+
+        <div className="cursor-pointer pr-0-5">
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+        <path d="M24 7.23607L27.5392 18.1287L27.7637 18.8197H28.4903H39.9434L30.6776 25.5517L30.0899 25.9787L30.3144 26.6697L33.8536 37.5623L24.5878 30.8303L24 30.4033L23.4122 30.8303L14.1464 37.5623L17.6856 26.6697L17.9101 25.9787L17.3224 25.5517L8.05655 18.8197H19.5097H20.2363L20.4608 18.1287L24 7.23607Z" stroke="#FFAB00" stroke-width="2" fill="">
+        </path>
+        </svg>
+        </div>
+
+        <div className="cursor-pointer pr-0-5">
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+        <path d="M24 7.23607L27.5392 18.1287L27.7637 18.8197H28.4903H39.9434L30.6776 25.5517L30.0899 25.9787L30.3144 26.6697L33.8536 37.5623L24.5878 30.8303L24 30.4033L23.4122 30.8303L14.1464 37.5623L17.6856 26.6697L17.9101 25.9787L17.3224 25.5517L8.05655 18.8197H19.5097H20.2363L20.4608 18.1287L24 7.23607Z" stroke="#FFAB00" stroke-width="2" fill="">
+        </path>
+        </svg>
+        </div>
+
+          </div>
+
           
       </div>
     )
